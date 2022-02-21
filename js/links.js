4ddbcdb13acc089087c398b131e28367e80f9789
Victor <Vmalho393@west-mec.edu>
2022-02-20 21:35:26 -0800

    Make the first links work

--- js/links.js
+++ js/links.js
@@ -1,8 +1,9 @@
 /* <- GOOGLE CLASSROOM -> */
-$('#classroom_img, #classroom_text').click(function() {
+$('#classroom_tile').click(function() {
     window.open('https://classroom.google.com/u/0/h');
 });
-/* <- FAMILY LINK -> */
-$('#familyLink_img, #familyLink_text').click(function() {
+/* <- FAMILYLINK -> */
+$('#familyLink_tile').click(function() {
     window.open('https://sm.west-mec.org/FAMILYLINK/Login.aspx');
 });
+//
