a13d434e2bcab83a6f2f0cb53871b17177bac958
Victor <Vmalho393@west-mec.edu>
2022-02-20 20:58:06 -0800

    first tiles

---
+++ js/links.js
@@ -0,0 +1,8 @@
+/* <- GOOGLE CLASSROOM -> */
+$('#classroom_img, #classroom_text').click(function() {
+    window.open('https://classroom.google.com/u/0/h');
+});
+/* <- FAMILY LINK -> */
+$('#familyLink_img, #familyLink_text').click(function() {
+    window.open('https://sm.west-mec.org/FAMILYLINK/Login.aspx');
+});
--- js/main.js
+++
@@ -1,3 +0,0 @@
-$('#classroom_text, #classroom_img').click(function() {
-    window.open('https://classroom.google.com/u/0/h');
-});
